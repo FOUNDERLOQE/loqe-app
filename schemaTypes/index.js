50077345c1197f2da0a1ef55f6ec8b4763725a25
Gauri <ntipl@Gauris-MacBook-Air.local>
2026-03-28 14:02:49 +0530

    Add hero video field to destination schema

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,5 +1,4 @@
 import destination from './destination'
 import experience from './experience'
-import stay from './stay'
 
-export const schemaTypes = [destination, experience, stay]
+export const schemaTypes = [destination, experience]
